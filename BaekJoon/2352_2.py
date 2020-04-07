cf0172bfecc033bfd86bd5efd15c34db07690aa2
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-04-08 00:25:30 +0900

    백준 3252번: 예시는 맞는게 결과는 틀리다고 나옴

--- BaekJoon/2352_2.py
+++ BaekJoon/2352_2.py
@@ -1,11 +1,22 @@
-endCount = int(input())
-endPoints = list(map(int, input().split()))
-endPointLst = []
+from operator import itemgetter
+num = int(input())
+inputEnd = list(map(int, input().split()))
+pointList = []
 
-for i in range(len(endPoints)):
-    endPointLst.append((i, endPoints[i]))
+for i in range(num):
+    start = i+1
+    end = inputEnd[i]
+    pointList.append((start, end, abs(start - end)))
 
-endPointLst = sorted(endPointLst, key = lambda x:x[0])
-endPointLst = sorted(endPointLst, key = lambda x:x[1])
+pointList.sort(key = itemgetter(2,0))
 
-print(endPointLst)
+lastOne = pointList[0]
+count = 1
+for i in range(1, num):
+    if pointList[i][0] > lastOne[0]:
+        if pointList[i][1] > lastOne[1]:
+            count += 1
+            lastOne = pointList[i]
+            print(pointList[i])
+
+print(count)
