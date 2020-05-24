5105b22b27e28cb354bb4944af6732be24b729e1
KimSuhyeon <kshvv975@gmail.com>
2020-05-24 13:04:55 +0900

    2020-05-24 2110번 공유기 개수

--- BaekJoon/2110.py
+++ BaekJoon/2110.py
@@ -1,32 +1,26 @@
 import  sys
 
 N, C = map(int, sys.stdin.readline().split())
-home = [int(sys.stdin.readline) for _  in range(N)]
+house = [int(sys.stdin.readline()) for _ in range(N)]
 
-home.sort()
+house.sort()
 
-def routerInstall(distance):
-    count = 1
-    cur_home = home[0] # 공유기가 설치된 집
+start = 1
+end = house[-1] - house[0]
+
+while start <= end:
+    mid = (start+end) // 2
+    wifi_count = 1
+    cur_house = house[0]
     for i in range(1, N):
-        if distance <= home[i] - cur_home: # 공유기를 설치하고자 하는 집과의 간격이 더 크기 때문에 공유기 설치 가능
-            count += 1
-            cur_home = home[i]
-    return count
+        if mid <= house[i] - cur_house:
+            wifi_count += 1
+            cur_house = house[i]
 
-# 공유기 개수를 넘겨줬을 때, 적절한 간격을 찾아주는 함수
-def BinarySerach(target):
-    start = 1
-    end = home[-1] - home[0]
+    if wifi_count< C:
+        end = mid - 1
+    elif wifi_count >= C:
+        answer = mid
+        start = mid + 1
+print(answer)
 
-    while start <= end:
-        mid = (start+end) // 2
-        router_cnt = routerInstall(mid)
-        if router_cnt < target: # 공유기 개수가 모자라면, 목표 간격을 좁힌다.
-            end = mid - 1
-        elif router_cnt >= target: # 공유기를 목표치 이상으로 설치할 수 있다면, 정답이 될 수 있다.
-            answer = mid
-            start = mid + 1
-    return answer
-
-print(BinarySerach(C))
