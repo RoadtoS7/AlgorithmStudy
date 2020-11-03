f084e233a114b827ea9c137ad99008e2e3f6da99
RoadtoS7 <kshvv975@gmail.com>
2020-11-03 22:55:15 +0900

    Add solution of 1920_수찾기.py

--- BaekJoon/1920_수찾기.py
+++ BaekJoon/1920_수찾기.py
@@ -1,11 +1,22 @@
-n = int(input())
-data = set(map(int, input().split(" ")))
+# n = int(input())
+# data = set(map(int, input().split(" ")))
+#
+# m = int(input())
+# value = list(map(int, input().split(" ")))
+# for index in value:
+#     inter = {index} & data
+#     if len(inter) == 0:
+#         print(0)
+#     else:
+#         print(1)
 
+n = int(input())
+array = set(map(int, input().split(" ")))
 m = int(input())
-value = list(map(int, input().split(" ")))
-for index in value:
-    inter = {index} & data
-    if len(inter) == 0:
-        print(0)
-    else:
+x = list(map(int, input().split(" ")))
+
+for i in x:
+    if i in array:
         print(1)
+    else:
+        print(0)
