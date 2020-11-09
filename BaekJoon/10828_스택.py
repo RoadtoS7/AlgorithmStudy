68f3cd86e5869caf80e09ff542264ab83ebb1e3b
RoadtoS7 <kshvv975@gmail.com>
2020-11-09 23:01:42 +0900

    Add 10828_스택.py

--- BaekJoon/10828_스택.py
+++ BaekJoon/10828_스택.py
@@ -1,27 +1,33 @@
-n = int(input())
+from sys import stdin
+times = int(stdin.readline())
 
-stack = [0] * n
-top_index = -1
+stack = []
+point = -1
 
-for _ in range(n):
-    command = list(input().split())
-    if command[0] == 'push':
-        top_index += 1
-        stack[top_index] = command[1]
-
-    elif command[0] == 'top':
-        print(stack[top_index])
-    elif command[0] == 'size':
-        print(top_index + 1)
-    elif command[0] == 'empty':
-        if top_index != -1:
-            print(0)
+for _ in range(times):
+    command = stdin.readline().strip().split(' ')
+    if command[0] == "push":
+        command[1] = int(command[1])
+        stack.append(command[1])
+        point += 1
+    elif command[0] == "pop":
+        if point >= 0:
+            print(stack[point])
+            del stack[point]
+            point -= 1
         else:
+            print(-1)
+
+    elif command[0] == "size":
+        print(point+1)
+    elif command[0] == "empty":
+        if point == -1:
             print(1)
-    else:
-        if top_index != -1:
-            print(stack[top_index])
-            stack[top_index] = 0
-            top_index -= 1
         else:
-            print(top_index)
+            print(0)
+    elif command[0] == "top":
+        if point >= 0:
+            print(stack[point])
+        else:
+            print(point)
+
