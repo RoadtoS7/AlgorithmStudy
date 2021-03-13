1051759cb8fd00346eda08b0e789516531ec4a2b
RoadtoS7 <kshvv975@gmail.com>
2021-03-13 20:27:37 +0900

    Add 더 효과적인 풀이 to 1181_단어정렬

--- BaekJoon/1181_단여정렬.py
+++ BaekJoon/1181_단여정렬.py
@@ -5,8 +5,15 @@ for i in range(N):
     word = input()
     words[i] = (len(word), word)
 
+
 words = list(set(words))
 words.sort()
 result = ""
 
 print('\n'.join(map(lambda x: x[1], words)))
+
+## 위의 과정을 아래와 같이 축소할 수 있다.
+# 핵심: key에 lambda를 이용해서 인자를 넣는다.
+words = list(set([input() for _ in range(int(input()))]))
+words.sort(key=lambda x: (len(x), x))
+print('\n'.join(words))
