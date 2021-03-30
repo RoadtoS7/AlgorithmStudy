c0e03ff7f92e583a8746c2c678d15283f7334c17
RoadtoS7 <kshvv975@gmail.com>
2021-03-30 22:15:52 +0900

    Add 9662_NQueen_210327.py

---
+++ Review/Baekjoon/9662_NQueen_210327.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def check(x):
+    for i in range(x):
+        if row[i] == row[x]:
+            return False
+        if abs(row[i] - row[x]) == x - i:
+            return False
+    return True
+
+def dfs(x):
+    global result
+    if x == n:
+        result += 1
+    else:
+        for i in range(n):
+            row[x] = i
+            if check(x):
+                dfs(x + 1)
+
+n = int(input())
+row = [0] * n
+result = 0
+dfs(0)
+print(result)
