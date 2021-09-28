1268c5794f4cdb437eafd82547f4c2f51de46c97
RoadtoS7 <kshvv975@gmail.com>
2021-09-29 00:41:33 +0900

    Fix Baekjoon/17779_게리맨더링2

--- BaekJoon/17779_게리맨더링2.py
+++ BaekJoon/17779_게리맨더링2.py
@@ -1,5 +1,4 @@
 from sys import stdin
-from collections import defaultdict
 input = stdin.readline
 
 N = int(input())
@@ -49,13 +48,13 @@ def get_dict5_end_c(x, y, d1, d2, r):
 
 
 def calc_popluation(x, y, N):
-    dict1_lst, dict2_lst, dict3_lst, dict4_lst = [], [], [], []
     min_value  = 1e9
     for d1 in range(1, y + 1):
         for d2 in range(1, N - y):
             if x + d1 + d1 >= N:
                 continue
 
+            dict1_lst, dict2_lst, dict3_lst, dict4_lst = [], [], [], []
             dict1, dict2, dict3, dict4 = 0, 0, 0, 0
             for r in range(N):
                 dict5_start_c = get_dict5_start_c(x, y, d1, d2, r)
@@ -65,61 +64,51 @@ def calc_popluation(x, y, N):
                 if dict5_start_c == None:
                     if r < x + d1:
                         dict1 += sum(map[r][:y + 1])
-                        dict1_lst.append((r, y))
+                        dict1_lst.extend(map[r][:y + 1])
                     else:
-                        dict3 += sum(map[r][:y])
-                        dict3_lst.append((r, y - 1))
+                        dict3 += sum(map[r][:y - d1 + d2])
+                        dict3_lst.extend(map[r][:y - d1 + d2])
 
                 else:
                     if r < x + d1:
                         dict1 += sum(map[r][:dict5_start_c])
-                        dict1_lst.append((r, dict5_start_c - 1))
+                        dict1_lst.extend(map[r][:dict5_start_c])
                     else:
                         dict3 += sum(map[r][:dict5_start_c])
-                        dict1_lst.append((r, dict5_start_c - 1))
+                        dict3_lst.extend(map[r][:dict5_start_c])
+
 
                 if dict5_end_c == None:
                     if r < x + d2:
                         dict2 += sum(map[r][y + 1:])
-                        dict2_lst.append((r, y + 1))
+                        dict2_lst.extend(map[r][y + 1:])
                     else:
-                        dict4 += sum(map[r][y:])
-                        dict4_lst.append((r, y))
-
+                        dict4 += sum(map[r][y - d1 + d2:])
+                        dict4_lst.extend(map[r][y - d1 + d2:])
                 else:
                     if r <= x + d2:
                         dict2 += sum(map[r][dict5_end_c + 1:])
-                        dict2_lst.append((r, dict5_end_c + 1))
+                        dict2_lst.extend(map[r][dict5_end_c + 1:])
                     else:
                         dict4 += sum(map[r][dict5_end_c + 1:])
-                        dict4_lst.append((r, dict5_end_c + 1))
+                        dict4_lst.extend(map[r][dict5_end_c + 1:])
 
 
             dict5 = total_map - sum([dict1, dict2, dict3, dict4])
-            diff =  max(dict1, dict2, dict3, dict4, dict5) - min(dict1, dict2, dict3, dict4, dict5)
-            if min_value > diff:
-                min_value = diff
-                print(x, y, d1, d2)
-            # min_value = min(min_value, diff)
+            diff = max(dict1, dict2, dict3, dict4, dict5) - min(dict1, dict2, dict3, dict4, dict5)
+            min_value = min(min_value, diff)
 
-        print(dict1_lst)
-        print(dict2_lst)
-        print(dict3_lst)
-        print(dict4_lst)
-        return min_value
+
+    return min_value
 
 
 
-# min_value = 1e9
-# for i in range(N):
-#     for j in range(N):
-#         result =  calc_popluation(i, j, N)
-#         if result:
-#             min_value = min(min_value, result)
+min_value = 1e9
+for i in range(N):
+    for j in range(N):
+        result =  calc_popluation(i, j, N)
+        if result:
+            min_value = min(min_value, result)
 
-
-min_value = calc_popluation(2, 4, N)
 print(min_value)
-
-
 
