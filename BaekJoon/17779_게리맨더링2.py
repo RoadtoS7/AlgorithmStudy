0a3135d615d8a81c1fffe26dfb64d4e2851b4617
RoadtoS7 <kshvv975@gmail.com>
2021-09-28 23:58:03 +0900

    Fix Baekjoon/17779_게리맨더링2

--- BaekJoon/17779_게리맨더링2.py
+++ BaekJoon/17779_게리맨더링2.py
@@ -4,39 +4,122 @@ input = stdin.readline
 
 N = int(input())
 map = [list(map(int, input().split())) for _ in range(N)]
+total_map = 0
+for lst in map:
+    total_map += sum(lst)
 
-min_value = 1e9
-def calc_population(x, y):
-    global min_value
 
-    district = defaultdict(int)
+def calc_dict_5(map, x, y, small, big):
+    result = 0
+    for i in range(0, small + 1):
+        result += sum(map[x + i][y - i: y + i + 1])
 
-    for d1 in range(N):
-        d2 = N - 1 - d1 - x
-        if d2 < 0:
-            continue
+    for i in range(1, big - small + 1):
+        result += sum(map[x + small + i][y - small + i : y + small + i])
 
-        if y - d1 < 0 or y + d2 >= N:
-            continue
+    for i in range(1, small + 1):
+        result += sum(map[x + big + i][y - 2 * small + big + i : y + big - i])
 
-        for r in range(N):
-            for c in range(N):
-                if 0 <= r < x + d1 and 0 <= c <= y:
-                    district[1] += map[r][c]
-                elif 0 <= r <= x + d2 and y < c <= N:
-                    district[2] += map[r][c]
-                elif x + d1 <= r <= N - 1 and 0 <= c < y - d1 + d2:
-                    district[3] += map[r][c]
-                elif x + d2 < r <= N - 1 and y - d1 + d2 <= c <= N - 1:
-                    district[4] += map[r][c]
+    return result
+
+def get_dict5_start_c(x, y, d1, d2, r):
+    if r < x:
+        return None
+    elif r <= x + d1:
+        diff = r - x
+        return y - diff
+    elif r <= x + d1 + d2:
+        diff = r - (x + d1)
+        return  (y - d1) + diff
+    else:
+        return None
+
+def get_dict5_end_c(x, y, d1, d2, r):
+    if r < x:
+        return None
+    elif r <= x + d2:
+        diff = r - x
+        return y + diff
+    elif r <= x + d1 + d2:
+        diff = r - (x + d2)
+        return (y + d2) - diff
+    else:
+        return None
+
+
+
+def calc_popluation(x, y, N):
+    dict1_lst, dict2_lst, dict3_lst, dict4_lst = [], [], [], []
+    min_value  = 1e9
+    for d1 in range(1, y + 1):
+        for d2 in range(1, N - y):
+            if x + d1 + d1 >= N:
+                continue
+
+            dict1, dict2, dict3, dict4 = 0, 0, 0, 0
+            for r in range(N):
+                dict5_start_c = get_dict5_start_c(x, y, d1, d2, r)
+                dict5_end_c = get_dict5_end_c(x, y, d1, d2, r)
+
+
+                if dict5_start_c == None:
+                    if r < x + d1:
+                        dict1 += sum(map[r][:y + 1])
+                        dict1_lst.append((r, y))
+                    else:
+                        dict3 += sum(map[r][:y])
+                        dict3_lst.append((r, y - 1))
+
                 else:
-                    district[5] += map[r][c]
-        min_value = min(min_value, max(district.values()) - min(district.values()))
+                    if r < x + d1:
+                        dict1 += sum(map[r][:dict5_start_c])
+                        dict1_lst.append((r, dict5_start_c - 1))
+                    else:
+                        dict3 += sum(map[r][:dict5_start_c])
+                        dict1_lst.append((r, dict5_start_c - 1))
 
-for i in range(N):
-    for j in range(N):
-        calc_population(i, j)
+                if dict5_end_c == None:
+                    if r < x + d2:
+                        dict2 += sum(map[r][y + 1:])
+                        dict2_lst.append((r, y + 1))
+                    else:
+                        dict4 += sum(map[r][y:])
+                        dict4_lst.append((r, y))
 
-print(min_value + 1)
+                else:
+                    if r <= x + d2:
+                        dict2 += sum(map[r][dict5_end_c + 1:])
+                        dict2_lst.append((r, dict5_end_c + 1))
+                    else:
+                        dict4 += sum(map[r][dict5_end_c + 1:])
+                        dict4_lst.append((r, dict5_end_c + 1))
+
+
+            dict5 = total_map - sum([dict1, dict2, dict3, dict4])
+            diff =  max(dict1, dict2, dict3, dict4, dict5) - min(dict1, dict2, dict3, dict4, dict5)
+            if min_value > diff:
+                min_value = diff
+                print(x, y, d1, d2)
+            # min_value = min(min_value, diff)
+
+        print(dict1_lst)
+        print(dict2_lst)
+        print(dict3_lst)
+        print(dict4_lst)
+        return min_value
+
+
+
+# min_value = 1e9
+# for i in range(N):
+#     for j in range(N):
+#         result =  calc_popluation(i, j, N)
+#         if result:
+#             min_value = min(min_value, result)
+
+
+min_value = calc_popluation(2, 4, N)
+print(min_value)
+
 
 
