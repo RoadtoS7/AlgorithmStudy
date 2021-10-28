b671881df2ff2cfe26b861aac1224a91ac8d1e62
RoadtoS7 <kshvv975@gmail.com>
2021-10-28 23:54:20 +0900

    Add Baekjoon/14500_테르로미노

---
+++ BaekJoon/14500_테르로미노/14500_테르로미노.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+max_value = -1
+dir = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+
+visited = [[False] * M for _ in range(N)]
+
+def dfs(r, c, count, value):
+    global max_value
+
+    if count >= 4:
+        max_value = max(max_value, value)
+        return
+
+    for dr, dc in dir:
+        nr, nc = r + dr, c + dc
+
+        if 0 <= nr < N and 0 <= nc < M:
+            if not visited[nr][nc]:
+                visited[nr][nc] = True
+                dfs(nr, nc, count + 1, value + graph[nr][nc])
+                visited[nr][nc] = False
+
+
+for r in range(N):
+    for c in range(M):
+        visited[r][c] = True
+        dfs(r, c, 1, graph[r][c])
+        visited[r][c] = False
+print(max_value)
