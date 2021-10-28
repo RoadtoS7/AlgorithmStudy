74bab15e44415d70f440b58dd4ffd2194704adac
RoadtoS7 <kshvv975@gmail.com>
2021-10-29 00:12:10 +0900

    Fix Baekjoon/14500_테르로미노
    ㅏ, ㅓ, ㅜ, ㅗ 모양의 블럭의 경우는 경우의 수도 포함하도록 수정함

--- BaekJoon/14500_테르로미노/14500_테르로미노.py
+++ BaekJoon/14500_테르로미노/14500_테르로미노.py
@@ -4,8 +4,15 @@ graph = [list(map(int, input().split())) for _ in range(N)]
 
 max_value = -1
 dir = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+dir_x = [
+    [(0, 1), (0, -1), (1, 0)],
+    [(0, 1), (0, -1), (-1, 0)],
+    [(0, 1), (1, 0), (-1, 0)],
+    [(0, -1), (1, 0), (-1, 0)],
+]
 
 visited = [[False] * M for _ in range(N)]
+
 
 def dfs(r, c, count, value):
     global max_value
@@ -29,4 +36,22 @@ for r in range(N):
         visited[r][c] = True
         dfs(r, c, 1, graph[r][c])
         visited[r][c] = False
+
+for r in range(N):
+    for c in range(M):
+
+        for dir in dir_x:
+            value = graph[r][c]
+            is_ok = True
+            for dr, dc in dir:
+                nr, nc = r + dr, c + dc
+
+                if 0 <= nr < N and 0 <= nc < M:
+                    value += graph[nr][nc]
+                else:
+                    is_ok = False
+
+            if is_ok:
+                max_value = max(max_value, value)
+
 print(max_value)
